931e3df6302b7228efa61576e69c9433d0df20ea
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-08 14:00:34 +0200

    profile form

---
+++ client/control_panel/profile/profile.js
@@ -0,0 +1,42 @@
+if (Meteor.isClient) {
+	Template.profile.helpers({
+		logo: function () {
+			var userModel = Meteor.users.findOne(Meteor.userId());
+			if (userModel) {
+				return userModel.profile.logo;
+			}
+		},
+		cover: function () {
+			var userModel = Meteor.users.findOne(Meteor.userId());
+			if (userModel) {
+				return userModel.profile.cover;
+			}
+		}
+	});
+	Template.profile.events({
+		'submit form': function (e, template) {
+			e.preventDefault();
+			if (template.find('#logoInput').files.length) {
+				var logoFile = template.find('#logoInput').files[0];
+				var logoReader = new FileReader();
+				logoReader.onload = function (e) {
+					Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.logo': e.target.result}});
+				};
+				logoReader.readAsDataURL(logoFile);
+			}
+			if (template.find('#coverInput').files.length) {
+				var coverFile = template.find('#coverInput').files[0];
+				var coverReader = new FileReader();
+				coverReader.onload = function (e) {
+					Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.cover': e.target.result}});
+				};
+				coverReader.readAsDataURL(coverFile);
+			}
+			var userData = {};
+			userData['name'] = template.find('#nameInput').value;
+			userData['description'] = template.find('#descriptionInput').value;
+			userData['address'] = template.find('#addressInput').value;
+			Meteor.users.update({_id: Meteor.userId()}, {$set: {profile: userData}});
+		}
+	});
+}
