5c6896bf15f945b82e2a500fb4a986dd17f3410e
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-08 14:35:07 +0200

    profile form

--- client/control_panel/profile/profile.js
+++ client/control_panel/profile/profile.js
@@ -36,6 +36,7 @@ if (Meteor.isClient) {
 			userData['profile.name'] = template.find('#nameInput').value;
 			userData['profile.description'] = template.find('#descriptionInput').value;
 			userData['profile.address'] = template.find('#addressInput').value;
+			userData['profile.directions'] = template.find('#directionsInput').value;
 			Meteor.users.update({_id: Meteor.userId()}, {$set: userData});
 		}
 	});
