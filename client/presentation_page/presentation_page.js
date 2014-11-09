bf5884cf899ce23647301ea8b6c2aba5f017094f
Artem Pervenonok <aperven@softserveinc.com>
2014-11-09 12:20:01 +0200

    CP style iImprovements

--- client/presentation_page/presentation_page.js
+++ client/presentation_page/presentation_page.js
@@ -6,7 +6,7 @@ if (Meteor.isClient) {
 		var hotelId = this.data.hotelId;
 		this.autorun(function () {
 			var hotel = Meteor.users.findOne(hotelId);
-			console.log(hotel);
+
 			if (hotel && hotel.profile && hotel.profile.location) {
 				gMap = new google.maps.Map(document.getElementById('map-canvas'), {
 					center: {lat: 0, lng: 0},
@@ -18,6 +18,23 @@ if (Meteor.isClient) {
 				gMarker = new google.maps.Marker({
 					map: gMap,
 					position: location
+				});
+
+				$.simpleWeather({
+					location: hotel.profile.location.lat + ', ' + hotel.profile.location.lng,
+					woeid: '',
+					unit: 'f',
+					success: function(weather) {
+						html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+						html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+						html += '<li class="currently">'+weather.currently+'</li>';
+						html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+				
+						$("#weather").html(html);
+					},
+					error: function(error) {
+						$("#weather").html('<p>'+error+'</p>');
+					}
 				});
 			}
 		});
